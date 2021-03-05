5364819aeddb365c8482e2af8b097624fd4e7f3b
Robert Ancell <robert.ancell@canonical.com>
2021-03-05 14:28:00 +1300

    Improve examples

--- example/example.dart
+++ example/example.dart
@@ -5,7 +5,10 @@ void main() async {
   var systemBus = DBusClient.system();
   var client = UDisksClient(systemBus);
   await client.connect();
+
   print('Running UDisks ${client.version}');
+  print('Supported filesystems: ${client.supportedFilesystems.join(' ')}');
+
   client.close();
   await systemBus.close();
 }
