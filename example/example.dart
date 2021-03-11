a0a0825c96803bc7380e37b1e266c5e0b0171824
Robert Ancell <robert.ancell@canonical.com>
2021-03-11 14:55:53 +1300

    Make the DBusClient parameter optional

--- example/example.dart
+++ example/example.dart
@@ -1,14 +1,11 @@
-import 'package:dbus/dbus.dart';
 import 'package:udisks/udisks.dart';
 
 void main() async {
-  var systemBus = DBusClient.system();
-  var client = UDisksClient(systemBus);
+  var client = UDisksClient();
   await client.connect();
 
   print('Running UDisks ${client.version}');
   print('Supported filesystems: ${client.supportedFilesystems.join(' ')}');
 
-  client.close();
-  await systemBus.close();
+  await client.close();
 }
